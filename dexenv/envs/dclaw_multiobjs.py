c282c62c9b6c4cab5c9ae96eb3a5b0c4d665a710
Anoop Sonar <anoopsonar30@gmail.com>
2025-10-16 12:09:18 -0400

    Single object training with the roller

--- dexenv/envs/dclaw_multiobjs.py
+++ dexenv/envs/dclaw_multiobjs.py
@@ -186,13 +186,13 @@ class DclawMultiObjs(DClawBase):
         object_assets, goal_assets, object_ids, object_tex_handles, object_ptds = [], [], [], [], []
         object_cat_ids = []
 
-        # THIS MAKES THE OBJECT A DUCK.
-        # object_id = None
-        # for index, object_urdf in enumerate(object_urdfs):
-        #     if '75443' in str(object_urdf):
-        #         object_id = index
-        #         break
-        # self.cfg.obj.object_id = object_id
+        # THIS MAKES THE OBJECT A ROLLER. '75443' for Duck.
+        object_id = None
+        for index, object_urdf in enumerate(object_urdfs):
+            if 't5n8oys5' in str(object_urdf):
+                object_id = index
+                break
+        self.cfg.obj.object_id = object_id
 
         if self.cfg.obj.object_id is not None:
             urdf_to_load = self.object_urdfs[self.cfg.obj.object_id]
